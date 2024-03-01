98c0d9595520185de5c4efc2c5e740d6d2e14f56
alannah lawlor <alannahlawlor@monzo.com>
2024-03-01 14:06:54 +0000

    completed project

--- codebases/simple_server/app.py
+++ codebases/simple_server/app.py
@@ -1,14 +1,15 @@
 import os
-from flask import Flask, request
+from flask import Flask, request, render_template
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def get_home():
-    return "Hello, world!"
+    return render_template('site.html')
 
 if __name__ == '__main__':
     app.run(
       debug=True,
       host="0.0.0.0" # Listen for connections _to_ any server
     )
+
